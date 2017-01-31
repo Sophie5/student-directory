8311262857761bb6af2bc5bac7e74a6c8f881eda
Sophie <sophie.k.mclean@btinternet.com>
2017-01-31 12:27:57 +0000

    Adding more information about students with hashes

--- directory.rb
+++ directory.rb
@@ -1,26 +1,26 @@
 
 #lests put all students into an array
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffry Baratheon",
-"Norman Bates"
+{name:"Dr. Hannibal Lecter", cohort: :november},
+{name:"Darth Vader", cohort: :november},
+{name:"Nurse Ratched", cohort: :november},
+{name:"Michael Corleone", cohort: :november},
+{name:"Alex DeLarge", cohort: :november},
+{name:"The wicked Witch of the West", cohort: :november},
+{name:"Terminator", cohort: :november},
+{name:"Freddy Krueger", cohort: :november},
+{name:"The Joker", cohort: :november},
+{name:"Joffry Baratheon", cohort: :november},
+{name:"Norman Bates", cohort: :november}
 ]
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
-  puts name
+def print(students)
+  students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
  end
 end
 def print_footer(names)
