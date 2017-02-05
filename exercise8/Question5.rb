780e3cc4b7170111bfeb5c5b1a4a6eed1f9c48c9
Sophie <sophie.k.mclean@btinternet.com>
2017-02-05 23:02:13 +0000

    Question 5 exercise 8

--- exercise8/Question5.rb
+++ exercise8/Question5.rb
@@ -1,17 +1,33 @@
+$stdout.sync=true
 def input_students
   puts  "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "To finish, just hit return six times "
  # creates an empty array
  students = []
  #gets the first name
 name = gets.chomp
+puts " Please enter age"
+age =gets.chomp
+puts "Please enter country of birth"
+country = gets.chomp
+puts "Please enter hobby"
+hobby = gets.chomp
+puts " Please enter favourite colour"
+colour =  gets.chomp
+puts "please put favourtie animal"
+animal = gets.chomp
  # while the name is not empty, repeats this code
  while !name.empty?  do
   # add the student hash to the array
- students << {name: name, cohort: :november}
+ students << {name: name, cohort: :november, age: age, country: country, hobby: hobby, colour: colour, animal: animal}
   puts "Now we have #{students.count} students"
   # get another name from the user
  name = gets.chomp
+ age =gets.chomp
+ country = gets.chomp
+ hobby = gets.chomp
+ colour = gets.chomp
+ animal = gets.chomp
 end
 # return students
  students
@@ -31,7 +47,7 @@ def print(students)
 students.map do |student|
   until  "#{student[:name]}".length == 0
 
-  puts  "#{student[:name]} (#{student[:cohort]} cohort)"
+  puts  "#{student[:name]} (#{student[:cohort]} cohort), age:#{student[:age]}, colour: #{student[:country]}, hobby: #{student[:hobby]}, favourite animal: #{student[:animal]} "
 break
 end
 end
